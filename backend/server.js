a590e6a8a1906e1b2a2ba3af15ebbde362bd339d
Saurabey ?? <saurabbaral03@gmail.com>
2024-06-13 00:30:16 +0545

    category api added.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const postRoutes = require("./routes/post");
+const categoryRoutes = require("./routes/category");
+
+const primsa = require("@prisma/client");
+
+const prisma = new primsa.PrismaClient();
 
 const app = express();
 
 app.use(express.json());
 
+// get posts by category
+app.get("/api/categories/:categoryId/posts", async (req, res) => {
+  const { categoryId } = req.params;
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        categoryId,
+      },
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
 // Routes
 app.use("/api/posts", postRoutes);
+app.use("/api/categories", categoryRoutes);
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
